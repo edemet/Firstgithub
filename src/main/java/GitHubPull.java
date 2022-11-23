cc8294d6c90dc1069ad32c2e3d8fb032edca2ab7
Emin <emin.demet@gmail.com>
2022-11-23 23:10:48 +0300

    v2

--- src/main/java/GitHubPull.java
+++ src/main/java/GitHubPull.java
@@ -2,5 +2,6 @@ public class GitHubPull {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I pushed first project to git hub");
     }
 }
